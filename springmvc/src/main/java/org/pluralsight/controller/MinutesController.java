c3a833fe6baa69da3b204a67359b1b00cde95a0d
Dominik Domagała <Dominik.Domagala@bzwbk.pl>
2017-11-29 21:40:56 +0100

    Demo: Post Data to Controller

--- springmvc/src/main/java/org/pluralsight/controller/MinutesController.java
+++ springmvc/src/main/java/org/pluralsight/controller/MinutesController.java
@@ -1,13 +1,17 @@
 package org.pluralsight.controller;
 
+import org.pluralsight.model.Exercise;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MinutesController {
 
 	@RequestMapping(value="/addMinutes")
-	public String addMinutes() {
+	public String addMinutes(@ModelAttribute ("exercise") Exercise exercise) {
+		
+		System.out.println("exercise: " + exercise.getMinutes());
 		return "addMinutes";
 	}
 }
